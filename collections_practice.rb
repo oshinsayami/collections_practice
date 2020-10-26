f752b3ee7e3811d7a91cc8400d77b907ecf4983d
Simran <mdrsimran@gmail.com>
2020-10-26 16:20:21 -0500

    commit tree 2b552eb0528e63c58b0f18b4a884877252644a75

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,78 @@
+require 'pry'
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort do|a,b|
+        b<=>a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a,b|
+        if a.length == b.length
+            0
+        elsif a.length < b.length
+            -1
+        elsif a.length > b.length
+            1
+        end
+    end
+end
+
+def swap_elements(array)
+    a=array[1]
+    array[1]=array[2]
+    array[2]=a
+    array
+
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    new_array=[]
+    arr=[]
+    array.each do |a|
+       arr = a.split('')
+       arr[2]="$"
+       new_array.push(arr.join(""))
+    end
+    # binding.pry
+    new_array
+end
+
+def find_a(array)
+    n=[]
+    new_array=[]
+    array.select do |a|
+        n = a.split('')
+        if n[0] == "a"
+            new_array=a
+        end
+    end
+
+end
+
+def sum_array(array)
+    array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+    new_array=[]
+    array.each_with_index do |num,idx|
+      
+        if idx != 1
+            new_array.push(num<<"s")
+        else
+            new_array.push(num)
+        end
+
+        
+    end
+    new_array
+
+end
